70fa7e348312096205fb7f84f632c5f3b865283c
hzgamer66 <64765794+hzgamer66@users.noreply.github.com>
2020-06-04 18:26:45 -0700

    Update record.py

--- record.py
+++ record.py
@@ -7,7 +7,7 @@ def record():
     FORMAT = pyaudio.paInt16
     CHANNELS = 1
     RATE = 44100
-    RECORD_SECONDS = 3
+    RECORD_SECONDS = 1
     WAVE_OUTPUT_FILENAME = "letters.wav"
 
     p = pyaudio.PyAudio()
