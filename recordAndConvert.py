f627882bb50e181e71017d00bf733b2b66185362
hzgamer66 <64765794+hzgamer66@users.noreply.github.com>
2020-06-05 15:54:22 -0700

    Update recordAndConvert.py

--- recordAndConvert.py
+++ recordAndConvert.py
@@ -6,6 +6,16 @@ import speechToText
 def get_letter():
     record.record()
     storing.upload_blob("hangman-audio-files", "letters.wav", "letters.wav")
-    return speechToText.sample_recognize('gs://hangman-audio-files/letters.wav')
+    speech = speechToText.sample_recognize('gs://hangman-audio-files/letters.wav')
+    if 'letter' in speech:
+        speech = list(speech)
+        for i in range(7):
+            del speech[0]
+        if ' ' in speech:
+            speech.remove(' ')
+        speech = ''.join(speech)
+
+    return speech
+
 
 
